9da120bf20f6f8a237497c3a67cc8d023230c48e
PROJECT279 <lenin110@163.com>
2019-11-08 19:07:34 +0800

    更新MySql版本

--- MemberManagementSystem/UserManage/ImportInfo.cs
+++ MemberManagementSystem/UserManage/ImportInfo.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using MemberManagementSystem.Init;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +16,41 @@ namespace MemberManagementSystem.UserManage
 {
     public partial class ImportInfo : Form
     {
+        MySqlConnection conn;
+        MySqlCommand cmd;
+
         public ImportInfo()
         {
             InitializeComponent();
+
+            //this.Text = LoadForm.TextList[int.Parse(this.Tag.ToString())];
+            LoadForm.LoadText(this);
+
+            conn = Program.ConDataBase();
+            cmd = new MySqlCommand();
+            cmd.Connection = conn;
         }
+
+        public static DataSet ReadExcel(string filepath)
+        {
+            try
+            {
+                string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filepath + ";Extended Properties='Excel 8.0;HDR=False;IMEX=1'";
+                OleDbConnection oleConn = new OleDbConnection(strConn);
+                oleConn.Open();
+                string sql = "select * from [Sheet1$]";
+                OleDbDataAdapter oleDaExcel = new OleDbDataAdapter(sql, oleConn);
+                DataSet oleDsExcel = new DataSet();
+                oleDaExcel.Fill(oleDsExcel, "table1");
+                oleConn.Close();
+                return oleDsExcel;
+            }
+            catch (Exception)
+            {
+                return null;
+                throw;
+            }
+        }
+
     }
 }
